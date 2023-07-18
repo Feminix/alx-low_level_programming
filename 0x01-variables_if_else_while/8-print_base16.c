573e1a78474fa8aef55cdd2495f24577c89426d3
Feminix <oluwafemielijah20@gmail.comgit config --global user.name Feminx>
2023-07-18 06:19:51 +0100

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+	
+	c = 'a';
+	k = 0;
+	while
+		(k < 10) {
+			putchar(k + '0');
+			k++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++
+		}
+	putchar('\n');
+	return (0);
+}

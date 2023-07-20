b851f41a68cec035d1236e5935135256ef4ae832
Feminix <oluwafemielijah20@gmail.comgit config --global user.name Feminx>
2023-07-20 23:57:33 +0100

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - function to check for a digit (0 through)
+ * @a: a - Variable
+ * @b: b - Variable
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+mul = a * b;
+return (mul);
+}

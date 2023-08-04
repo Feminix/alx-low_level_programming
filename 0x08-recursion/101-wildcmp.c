eb4bdaf2a160f52f3ac3a0c8c8ce37a7853d804d
Your Name <oluwafemielijah20@gmail.com>
2023-08-03 22:38:09 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * wildcap - compare strings
- * @s1: pointer to string parans
- * @s2: pointer to string parans
+ * wildcmp - compare strings
+ * @s1: pointer to string params
+ * @s2: pointer to string params
  * Return: 0
  */
 
-int wildcap(char *s1, char *s2)
+int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0')
 	{
